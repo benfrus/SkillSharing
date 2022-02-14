9ff8083f9759d717df275168e91cde1b4fa98d27
Benito <al315702@uji.es>
2022-02-14 18:53:17 +0100

    Añade la estructura del proyecto

---
+++ src/main/java/es/uji/ei1027/SkillSharing/SkillSharingApplication.java
@@ -0,0 +1,23 @@
+package es.uji.ei1027.SkillSharing;
+
+import java.util.logging.Logger;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SkillSharingApplication implements CommandLineRunner {
+
+	private static final Logger log = Logger.getLogger(SkillSharingApplication.class.getName());
+
+	public static void main(String[] args) {
+		// Auto-configura l'aplicació
+		new SpringApplicationBuilder(SkillSharingApplication.class).run(args);
+	}
+
+	// Funció principal
+	public void run(String... strings) throws Exception {
+		log.info("Ací va el meu codi");
+	}
+}
